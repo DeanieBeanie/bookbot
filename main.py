523b66800a7be69b5293e66b7864ec4fa39bc09d
DeanieBeanie <dtbleh445@gmail.com>
2025-02-08 00:05:53 -0800

    added 'chars_dict_to_sorted' function, learned about the .sort() and sorted() functions!

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     contents = get_contents(book_path)
     char_dict = char_count(contents)
+    char_dict_sorted = chars_dict_to_sorted(char_dict)
 
     #insert functions to run below
     #print(f"The word count is: {word_count(contents)}!")
     #print(f"The character count is: {char_count(contents)}")
-    report(book_path, contents, char_dict)
+    #REPORT WORKS!!! WOOHOO
+    report(book_path, contents, char_dict_sorted)
 
 
 def get_contents(path):
@@ -30,6 +32,7 @@ def get_word_count(text):
 
     return num_of_words
 
+# char_count returns a dictionary in the format of {key: ch, value: count}, i.e. {"a", 27}
 def char_count(text):
     chars = {}
     lowercase_text = text.lower()
@@ -44,7 +47,7 @@ def char_count(text):
     return chars
 
 # make a report() function that prints the title of the book, wordcount, and character counts!
-# report()
+# report() will sort the char counts by most to least, using inspiration from solution code~
 def report(title, text, char_dict):
     book_path = title
     word_count = get_word_count(text)
@@ -52,14 +55,26 @@ def report(title, text, char_dict):
     print(f"--- Begin report of {book_path}---")
     print(f"{word_count} words found in the document \n")
     for key in char_dict:
-        if key.isalpha():
+        #print(key)
+        if str(key).isalpha():
         #print(f"char: {key}")
         #print (f"char count: {char_dict[key]}")
             print(f"The '{key}' character was found {char_dict[key]} times")
 
     print (f"--- End report ---")
 
+def chars_dict_to_sorted(char_dict):
+    sorted_list = []
+    sorted_dict = {}
+    for ch in char_dict: #sorting in descending order of "num" value
+        if ch.isalpha():
+            sorted_list.append({"char": ch, "num": char_dict[ch]})
+    sorted_list.sort(key=lambda x:x["num"],reverse=True)
 
+    for key in sorted_list:
+        sorted_dict[key["char"]] = key["num"]
+
+    return sorted_dict
 
 
 main()
