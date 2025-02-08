79e520796b64b225cfd4a62b4c4cbae0d0787a70
DeanieBeanie <dtbleh445@gmail.com>
2025-02-07 23:30:05 -0800

    added report() function

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     contents = get_contents(book_path)
     char_dict = char_count(contents)
+
     #insert functions to run below
     #print(f"The word count is: {word_count(contents)}!")
-    print(f"The character count is: {char_count(contents)}")
+    #print(f"The character count is: {char_count(contents)}")
+    report(book_path, contents, char_dict)
 
 
 def get_contents(path):
@@ -17,7 +19,7 @@ def get_contents(path):
     #...
 
 #method for counting words
-def word_count(text):
+def get_word_count(text):
     num_of_words = 0
     #the textfile of "frankenstein" is one single string!
     #we'll use the .split() method to turn it into a list of x words. spliting at the white spaces.
@@ -42,7 +44,21 @@ def char_count(text):
     return chars
 
 # make a report() function that prints the title of the book, wordcount, and character counts!
-def report()
+# report()
+def report(title, text, char_dict):
+    book_path = title
+    word_count = get_word_count(text)
+    #words = word_count(text)
+    print(f"--- Begin report of {book_path}---")
+    print(f"{word_count} words found in the document \n")
+    for key in char_dict:
+        if key.isalpha():
+        #print(f"char: {key}")
+        #print (f"char count: {char_dict[key]}")
+            print(f"The '{key}' character was found {char_dict[key]} times")
+
+    print (f"--- End report ---")
+
 
 
 
